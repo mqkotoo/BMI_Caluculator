2de051ac846a517a0c3e965d1c206e91e5467d64
mqkotoo <ilikekiss@icloud.com>
2022-03-07 00:19:50 +0900

    setting UI frame

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const CardsColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -13,8 +17,64 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text("Body Text"),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                // 1
+                Expanded(
+                  child: Card(color: CardsColor,),
+                ),
+                Expanded(
+                  child: Card(color: CardsColor,),
+                ),
+              ],
+            ),
+          ),
+          // 2
+          Expanded(
+            child: Card(color: CardsColor,),
+          ),
+          // 3
+          Expanded(
+            child: Row(
+              children: [
+                // 1
+                Expanded(
+                  child: Card(color: CardsColor,)
+                ),
+                Expanded(
+                  child: Card(color: CardsColor,),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+
+  Card({required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: color,
+        borderRadius: BorderRadius.circular(10.0)
       ),
     );
   }
